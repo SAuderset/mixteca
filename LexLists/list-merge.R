7aeaf7ff06b65a33fcf1d6e49f26f39611a82386
Sandra <40177170+audersets@users.noreply.github.com>
2020-05-08 14:12:50 +0200

    updated full list

--- LexLists/list-merge.R
+++ LexLists/list-merge.R
@@ -34,33 +34,33 @@ head(m1)
 
 
 # merge with josserand
-m2 <- full_join(m1, joss, by = c("CONCEPTICON_ID", "ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS = CONCEPTICON_GLOSS.x, ADD_CONCEPT, ID_EWC:ID_MAT, ID_JOSS = NUMBER, SPANISH_EWC:SPANISH_MAT, SPANISH_JOSS = SPANISH, ENGLISH_EWC:ENGLISH_MAT, ENGLISH_JOSS = ENGLISH)
+m2 <- full_join(m1, joss, by = c("CONCEPTICON_ID", "CONCEPTICON_GLOSS" ,"ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS, ADD_CONCEPT, ID_EWC:ID_MAT, ID_JOSS = NUMBER, SPANISH_EWC:SPANISH_MAT, SPANISH_JOSS = SPANISH, ENGLISH_EWC:ENGLISH_MAT, ENGLISH_JOSS = ENGLISH)
 glimpse(m2)
 
 # merge with durr
-m3 <- full_join(m2, durr, by = c("CONCEPTICON_ID", "ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS = CONCEPTICON_GLOSS.x, ADD_CONCEPT, ID_EWC:ID_JOSS, ID_DURR = NUMBER, SPANISH_EWC:SPANISH_JOSS, ENGLISH_EWC:ENGLISH_JOSS, ENGLISH_DURR = ENGLISH)
+m3 <- full_join(m2, durr, by = c("CONCEPTICON_ID", "CONCEPTICON_GLOSS" ,"ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS, ADD_CONCEPT, ID_EWC:ID_JOSS, ID_DURR = NUMBER, SPANISH_EWC:SPANISH_JOSS, ENGLISH_EWC:ENGLISH_JOSS, ENGLISH_DURR = ENGLISH)
 glimpse(m3)
 
 # merge with swanton-mendoza
-m4 <- full_join(m3, swme, by = c("CONCEPTICON_ID", "ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS = CONCEPTICON_GLOSS.x, ADD_CONCEPT, ID_EWC:ID_DURR, ID_SWME = NUMBER, SPANISH_EWC:SPANISH_JOSS, SPANISH_SWME = SPANISH, ENGLISH_EWC:ENGLISH_DURR)
+m4 <- full_join(m3, swme, by = c("CONCEPTICON_ID", "CONCEPTICON_GLOSS" ,"ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS, ADD_CONCEPT, ID_EWC:ID_DURR, ID_SWME = NUMBER, SPANISH_EWC:SPANISH_JOSS, SPANISH_SWME = SPANISH, ENGLISH_EWC:ENGLISH_DURR)
 glimpse(m4)
 
 # merge with padgett
-m5 <- full_join(m4, pdt, by = c("CONCEPTICON_ID", "ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS = CONCEPTICON_GLOSS.x, ADD_CONCEPT, ID_EWC:ID_SWME, ID_PADG = NUMBER, SPANISH_EWC:SPANISH_SWME, SPANISH_PADG = SPANISH, ENGLISH_EWC:ENGLISH_DURR, ENGLISH_PADG = ENGLISH)
+m5 <- full_join(m4, pdt, by = c("CONCEPTICON_ID", "CONCEPTICON_GLOSS" ,"ADD_CONCEPT")) %>% select(CONCEPTICON_ID, CONCEPTICON_GLOSS, ADD_CONCEPT, ID_EWC:ID_SWME, ID_PADG = NUMBER, SPANISH_EWC:SPANISH_SWME, SPANISH_PADG = SPANISH, ENGLISH_EWC:ENGLISH_DURR, ENGLISH_PADG = ENGLISH)
 glimpse(m5)
 
-# check for duplicates
-anyDuplicated(m5)
-
-?rowsum
-rowsum(m5, m5$ID_EWC)
+# check for duplicates in concepticon id
+duplid <- m5 %>% group_by(CONCEPTICON_ID) %>% 
+  filter(n()>1)
+duplid
+# house and maize in matsukawa
 
 # add column that shows in how many lists a concept appears
 m6 <- m5 %>% mutate(COVERAGE = (rowSums(!is.na(select(., one_of(c("ID_EWC", "ID_MAT", "ID_JOSS", "ID_DURR", "ID_SWME", "ID_PADG")))))))
 glimpse(m6)
 
 table(m6$COVERAGE)
-# approx. 333 concepts that are in more than 1 list
+# 325 concepts that are in more than 1 list
 
 # export for manual clean up and inspection
 write_csv(m6, "/Users/auderset/Documents/GitHub/mixteca/LexLists/full-list-preproc.csv")
