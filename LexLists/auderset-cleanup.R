dba681f5e33189b1de4ff8cef554d3c5670a6c55
Sandra <40177170+audersets@users.noreply.github.com>
2020-05-11 15:58:18 +0200

    updated lists

---
+++ LexLists/auderset-cleanup.R
@@ -0,0 +1,21 @@
+
+library(tidyverse)
+
+aud <- read_csv("/Users/auderset/Documents/GitHub/mixteca/LexLists/Auderset-2020-NA.csv")
+
+glimpse(aud)
+tail(aud)
+
+# delete empty rows, delete white space, glosses to lower case
+aud1 <- aud %>% slice(1:200) %>% 
+  mutate_at(vars(ENGLISH, SPANISH), str_to_lower) %>%
+  mutate_if(is.double, as.integer) %>%
+  mutate_if(is.character, trimws) %>% 
+  mutate_if(is.character, as.factor)
+glimpse(aud1)  
+
+# reorder columns
+aud2 <- aud1 %>% select(ID, CATEGORY, SPANISH, ENGLISH, CONCEPTICON_ID, CONCEPTICON_GLOSS, ADD_CONCEPT, COVERAGE, ID_EWC, ID_JOSS, ID_DURR, ID_SWME, ID_MAT, ID_PADG, everything())
+glimpse(aud2)
+
+# export for final cleanup
